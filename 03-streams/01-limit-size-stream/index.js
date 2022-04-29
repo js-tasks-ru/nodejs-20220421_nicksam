f06f0f1afb8a0e4f1fb9ce58adecd840671b6b05
Nick Sam <Noizerg@Gmail.com>
2022-04-29 12:18:00 +0300

    1/5

--- 03-streams/01-limit-size-stream/index.js
+++ 03-streams/01-limit-size-stream/index.js
@@ -1,10 +1,10 @@
 const LimitSizeStream = require('./LimitSizeStream');
 const fs = require('fs');
 
-const limitedStream = new LimitSizeStream({limit: 8, encoding: 'utf-8'}); // 8 байт
+const limitedStream = new LimitSizeStream({ limit: 8, encoding: 'utf-8' }); // 8 байт
 const outStream = fs.createWriteStream('out.txt');
 
-limitedStream.pipe(outStream);
+limitedStream.on('error', (err) => console.log(err.code)).pipe(outStream);
 
 limitedStream.write('hello'); // 'hello' - это 5 байт, поэтому эта строчка целиком записана в файл
 
