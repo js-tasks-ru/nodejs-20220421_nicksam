8ad306e9fc1a4afffd0106c027d0aa654013f931
Nick Sam <Noizerg@Gmail.com>
2022-05-29 13:45:09 +0300

    mongo 1

--- 06-mongodb-mongoose/01-schema-model/models/Category.js
+++ 06-mongodb-mongoose/01-schema-model/models/Category.js
@@ -1,10 +1,19 @@
 const mongoose = require('mongoose');
+const { stringify } = require('uuid');
 const connection = require('../libs/connection');
 
 const subCategorySchema = new mongoose.Schema({
+  title: { type: String, required: true },
 });
 
+const SubCategory = connection.model('Subcategory', subCategorySchema);
 const categorySchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  subcategories: [subCategorySchema],
 });
+const Category = connection.model('Category', categorySchema);
 
-module.exports = connection.model('Category', categorySchema);
+console.log(Category.schema.obj);
+console.log('title', SubCategory.schema.path('title').instance);
+
+module.exports = Category;
