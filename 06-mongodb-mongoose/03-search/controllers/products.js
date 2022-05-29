a3a67e37eca032d1db41b88964f42f3550baa587
Nick Sam <Noizerg@Gmail.com>
2022-05-29 15:47:08 +0300

    mongo done

--- 06-mongodb-mongoose/03-search/controllers/products.js
+++ 06-mongodb-mongoose/03-search/controllers/products.js
@@ -1,3 +1,22 @@
+const mapper = require('../mappers/product');
+const modelProd = require('../models/Product');
+
 module.exports.productsByQuery = async function productsByQuery(ctx, next) {
-  ctx.body = {products: []};
+  const { query } = ctx.query;
+  // const result = modelProd.aggregate([
+  //   { $match: { $text: { $search: query } } },
+  //   { $sort: { score: { $meta: 'textScore' } } },
+  // ]);
+  const out = {};
+  out.products = [];
+
+  const result = await modelProd
+    .find({ $text: { $search: query } }, { score: { $meta: 'textScore' } })
+    .sort({ score: { $meta: 'textScore' } });
+  if (result.length == 0) {
+    ctx.body = out;
+    return next();
+  }
+  result.forEach((item) => out.products.push(mapper(item)));
+  ctx.body = out;
 };
